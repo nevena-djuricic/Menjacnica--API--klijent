aa059698219fca033e97bf94d1fc2591f1f1bc15
nevena-djuricic <dj.nevena@hotmail.com>
2015-05-20 13:44:32 +0200

    Dodata main metoda

---
+++ src/rs/fonis/main/Main.java
@@ -0,0 +1,14 @@
+package rs.fonis.main;
+
+import rs.fonis.azuriranje.AzuriranjeKursneListe;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AzuriranjeKursneListe a = new AzuriranjeKursneListe();
+		
+		a.azurirajValute();
+		
+	}
+	
+}
